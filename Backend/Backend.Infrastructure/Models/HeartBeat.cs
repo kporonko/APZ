eb4a30273359a43da8ce8a5f08157a57f9343187
Kyryl Poronko <kporonko@managed-code.com>
2023-04-15 15:19:09 +0300

    Models added

---
+++ Backend/Backend.Infrastructure/Models/HeartBeat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Models
+{
+    public class HeartBeat
+    {
+        public int Id { get; set; }
+        public DateTime HeartBeatDate { get; set; }
+        public int Value { get; set; }
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
