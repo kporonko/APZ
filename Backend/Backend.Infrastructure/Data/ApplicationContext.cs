01fe6c60a10dad274242cd66e30eb2c25f81e2e4
Kyryl Poronko <kporonko@managed-code.com>
2023-04-15 15:10:39 +0300

    Project structure, configuration is added

---
+++ Backend/Backend.Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,55 @@
+﻿using backend.Infrastructure.Configuration;
+using backend.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace backend.Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CoachConfiguration());
+            modelBuilder.ApplyConfiguration(new PlayerConfiguration());
+            modelBuilder.ApplyConfiguration(new TrainingConfiguration());
+            modelBuilder.ApplyConfiguration(new TeamConfiguration());
+
+            modelBuilder.Entity<Coach>()
+                .HasOne(c => c.Team)
+                .WithOne(t => t.Coach)
+                .HasForeignKey<Team>(t => t.CoachId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Player>()
+                .HasOne(p => p.Team)
+                .WithMany(t => t.Players)
+                .HasForeignKey(p => p.TeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Training>()
+                .HasOne(t => t.Player)
+                .WithMany(p => p.Trainings)
+                .HasForeignKey(t => t.PlayerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+        }
+    }
+}
