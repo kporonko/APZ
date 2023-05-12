c88833f201e93d083d7def616c1a5150d9e60bde
Kyryl Poronko <poronko2045@gmail.com>
2023-05-13 00:07:11 +0300

    Team page markup implemented

--- Frontend/frontend/src/components/TeamComponent.tsx
+++ Frontend/frontend/src/components/TeamComponent.tsx
@@ -3,8 +3,27 @@ import {ITeam} from "../interfaces/ITeam";
 
 const TeamComponent = (props:{team: ITeam}) => {
     return (
-        <div>
-
+        <div className={"team-wrapper"}>
+            <div className={"header-my-team"}>
+                My Team
+            </div>
+            <div className={"team-page-top"}>
+                <div className={"team-page-top-image"}>
+                    <img className={"team-image"} src={props.team.image} alt=""/>
+                </div>
+                <div className={"team-page-top-info"}>
+                    <h1>{props.team.name}</h1>
+                    <p>{props.team.description}</p>
+                </div>
+                <div className="team-btns-wrapper">
+                    <div className="edit-team-button">
+                        Edit team
+                    </div>
+                    <div className="delete-team-button">
+                        Delete team
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
